8444255208026a159f7477aec016bb0574fdf79e
Bogart19 <stephanie@daily.co>
2023-03-14 11:52:34 -0700

    Add sample tests

---
+++ sum.test.js
@@ -0,0 +1,20 @@
+const sum = require('./sum');
+
+test('should pass', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('should fail', () => {
+    expect(sum(1, 2)).toBe(5);
+})
+
+test('should be flaky', () => {
+    
+    let randomNumber = Math.floor(Math.random() * 10);
+    if (randomNumber % 2 == 0) {
+        expect(sum(1, 2)).toBe(3)
+    }
+    else {
+        expect(sum(1, 2)).toBe(0)
+    }
+})
